bf7202875a417c629cc2ebf7240dbed56e1f6752
Claire Martin <claire.e.martin15@gmail.com>
2023-10-18 11:50:28 +0100

    upload 2

--- server/server.js
+++ server/server.js
@@ -1,2 +1,11 @@
 const express = require('express');
 const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello from the server!');
+  });
+
+
+app.listen(9000, function () {
+    console.log('app is working')
+})
